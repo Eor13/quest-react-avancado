0dd9999915497d41543f276a57c3573d664d0989
Everson de Oliveira <eor1309@gmail.com>
2023-02-04 13:26:31 -0300

    Correção de estrutura de pastas

---
+++ src/components/button/index.js
@@ -0,0 +1,15 @@
+import Pokebola from "../../images/pokebola.png";
+import { useContext } from "react";
+import { ThemeContext } from "../../services/theme-context";
+import * as Styled from './style';
+
+const TogglerButton = (props) => {
+    const { theme } = useContext(ThemeContext)
+    return(
+        <Styled.Button {...props} style={{color: theme.color, background: theme.background}}>
+          <Styled.Img src={Pokebola} alt="Botão de Troca de Tema"/>
+        </Styled.Button>
+    )
+}
+
+export { TogglerButton }
